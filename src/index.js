ba67a88309cd73ba815d31cfb166dcd9da462282
Elena Piaggio <elenapiaggio@gmail.com>
2020-05-08 20:37:37 +0200

    call the calculateWinner() function in render of Board component to know the winner

--- src/index.js
+++ src/index.js
@@ -41,7 +41,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: X ${this.state.xIsNext ? 'X' : 'O'}`;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner){
+      status = `Winner: ${winner}`;
+    } else{
+      status = `Next player: ${this.state.xIsNext ? 'X' : '0'}`;
+    }
+    
     return(
       <div>
         <div className="status">{status}</div>
