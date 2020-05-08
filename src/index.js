e444be92d02293c1f36d12b45c4f35d11a8d5fcf
Elena Piaggio <elenapiaggio@gmail.com>
2020-05-08 18:43:08 +0200

    UPDATE square state

--- src/index.js
+++ src/index.js
@@ -3,21 +3,13 @@ import ReactDOM from "react-dom";
 import "./style/index.css";
 
 class Square extends React.Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      value: null
-    }
-  }
-
   render() {
     return (
       <button 
         className="square"
-        onClick = { () => this.setState({value: 'X'}) }
+        onClick = { () => this.props.onClick() }
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -30,8 +22,22 @@ class Board extends React.Component {
       squares: Array(9).fill(null),
     }
   }
+
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState( 
+      {squares: squares} 
+    );
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square 
+        value = { this.state.squares[i] } 
+        onClick = { () => this.handleClick(i) }
+      />
+      );
   }
 
   render() {
