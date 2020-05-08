a7cac31ba94bad810aa26b1c803dcd6f243ab758
Elena Piaggio <elenapiaggio@gmail.com>
2020-05-08 19:06:27 +0200

    Every time the player makes a move, the xIsNext boolean will be iverted

--- src/index.js
+++ src/index.js
@@ -24,10 +24,11 @@ class Board extends React.Component {
 
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState( 
-      {squares: squares} 
-    );
+    squares[i] = this.state.xIsNext ? 'X' : '0';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
