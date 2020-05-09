46825809fe8c88f6a33ac3b64196ab1f1ca8dec9
Elena Piaggio <elenapiaggio@gmail.com>
2020-05-09 11:09:44 +0200

    MOVE handleClick method of Board component to Game component

--- src/index.js
+++ src/index.js
@@ -14,20 +14,6 @@ function Square(props) {
 }
  
 class Board extends React.Component {
-  handleClick(i){
-    const squares = this.state.squares.slice();
-
-    if(calculateWinner(squares) || squares[i]){
-      return;
-    }
-    
-    squares[i] = this.state.xIsNext ? 'X' : '0';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
   renderSquare(i) {
     return (
       <Square 
@@ -69,6 +55,20 @@ class Game extends React.Component {
       }],
       xIsNext: true,
     }
+  }
+
+  handleClick(i){
+    const squares = this.state.squares.slice();
+
+    if(calculateWinner(squares) || squares[i]){
+      return;
+    }
+    
+    squares[i] = this.state.xIsNext ? 'X' : '0';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   render() {
